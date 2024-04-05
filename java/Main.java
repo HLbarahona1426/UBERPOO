9ecd073aeaf83d12669e7c4f485567e2997bbff1
Henry Barahona <henrybarahona84@gmail.com>
2024-04-05 13:44:53 -0600

    Agregando Actividad

---
+++ java/Main.java
@@ -0,0 +1,13 @@
+class Main {
+    
+    public static void main (String[] args) {
+        System.out.println("Inicializando...");
+        system.out println("Car...");
+        Car car= new Car ("AMQ123", new Account("Andres Loiza", "ADN12345", "andres1@hotmail.com","12356"));
+        car.passenger = 4;
+        car.printDataCar();
+        System.out.print1n("Uberx....");
+        Uberx uberx = new UberX
+        ("MKL185", new Account ("Maria Loyola", "JKL12365", "marial@hotmail.com", "125478"), "Toyota", "Corolla");
+    }
+}
